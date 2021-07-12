22f3d21e40b325d8e8b3ab32a257c8dec1c3cc5a
Penguin.jpg <sky026912@gmail.com>
2021-07-12 16:53:16 +0800

    add Search a 2D Matrix and modify Search a 2D Matrix II

--- Medium/Search_a_2D_Matrix_II_Solution1.cpp
+++ Medium/Search_a_2D_Matrix_II_Solution1.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) { 
         /*
-            我的方法1
-            時間複雜度O(mlog(n))(m:列數、n:行數)、空間複雜度O(1)
+            我的方法
+            時間複雜度O(mlog(n))(m:row的數量、n:col的數量)、空間複雜度O(1)
+            對每個row做二元搜尋
         */
 
         for(int i=0;i<matrix.size();i++)
@@ -11,6 +12,7 @@ public:
             int begin=0;
             int end=matrix[0].size()-1;
             int mid=(begin+end)/2;
+            
             while(begin<=end)
             {
                 if(matrix[i][mid]==target)
@@ -27,6 +29,7 @@ public:
                 mid=(begin+end)/2;
             }
         }
+        
         return false;
     }
 };
--- Medium/Search_a_2D_Matrix_II_Solution2.cpp
+++ Medium/Search_a_2D_Matrix_II_Solution2.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         /*
-            我的方法2
-            時間複雜度O(n)、空間複雜度O(1)
+            參考的方法
+            時間複雜度O(m+n)(m:row的數量、n:col的數量)、空間複雜度O(1)
+            先從每列的最大開始看，如果比target大，就往左邊那行找，
+            如果比target小，就往下一列找
         */
 
         int i=0;
